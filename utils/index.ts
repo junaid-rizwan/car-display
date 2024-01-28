a4a4514fa0d8d94fc3b8305e402415a1277589f0
Shaik Junaid Rizwan <junaidrizwan2003@gmail.com>
2024-01-28 10:12:37 +0530

    first

---
+++ utils/index.ts
@@ -0,0 +1,58 @@
+import { CarProps, FilterProps } from "@/types";
+
+
+export async function fetchCars(filters :FilterProps){
+    const { manufacturer, year, model, limit, fuel } = filters;
+    const headers ={
+        'X-RapidAPI-Key': 'e09d23f7ddmshf3046693a898f50p1d582djsn1b887c484553',
+		    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch( `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{headers:headers});
+
+    const result=await response.json();
+    return result;
+}
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+
+  export const generateCarImage=(car:CarProps,angle?:String)=>{
+
+    const url=new URL('https://cdn.imagin.studio/getimage');
+
+    const {make,year,model}=car;
+
+    url.searchParams.append('customer','hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+   
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+  }
+
+  export const updateSearchParams = (type: string, value: string) => {
+  
+    const searchParams = new URLSearchParams(window.location.search);
+  
+
+    searchParams.set(type, value);
+  
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  
+    return newPathname;
+  };
